25aeb2d6cbd55018500896071edff32286546fd7
DrSmogs <james,shaw000@gmail.com>
2017-01-17 22:02:03 +1100

    Cleaned up code and added a single message script - included Daves changes to stanza.py

--- iq3/__init__.py
+++ iq3/__init__.py
@@ -1,9 +1,7 @@
 from sleekxmpp.plugins.base import register_plugin, BasePlugin
 
-from iq3.stanza import Current, diagnostic_tuner, current_viewing
+from iq3.stanza import current_programme, diagnostic_tuner, current_viewing
 from iq3.current import iq3
 
 
 register_plugin(iq3)
-
-
